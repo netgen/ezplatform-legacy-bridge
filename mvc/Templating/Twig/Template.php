ca13b113bb616968d2ba36a3fff69bc1802c92d6
Edi Modrić <edi.modric@gmail.com>
2024-05-13 16:50:39 +0200

    Fix issues with non matching interfaces after Twig 3.9

--- mvc/Templating/Twig/Template.php
+++ mvc/Templating/Twig/Template.php
@@ -31,40 +31,21 @@ class Template extends BaseTemplate
         $this->legacyEngine = $legacyEngine;
     }
 
-    /**
-     * Renders the template with the given context and returns it as string.
-     *
-     * @param array $context An array of parameters to pass to the template
-     *
-     * @return string The rendered template
-     */
-    public function render(array $context)
+    public function render(array $context): string
     {
         return $this->legacyEngine->render($this->templateName, $context);
     }
 
-    /**
-     * Displays the template with the given context.
-     *
-     * @param array $context An array of parameters to pass to the template
-     * @param array $blocks  An array of blocks to pass to the template
-     */
-    public function display(array $context, array $blocks = [])
+    public function display(array $context, array $blocks = []): void
     {
         echo $this->render($context);
     }
 
-    /**
-     * @return string
-     */
     public function getTemplateName()
     {
         return $this->templateName;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getDebugInfo()
     {
         return [];
@@ -75,9 +56,6 @@ class Template extends BaseTemplate
         return new Source('', '');
     }
 
-    /**
-     * {@inheritdoc}
-     */
     protected function doDisplay(array $context, array $blocks = [])
     {
     }
